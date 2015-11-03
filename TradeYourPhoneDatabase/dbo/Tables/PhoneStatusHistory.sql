483675ece01672d18be96449e2f2d84f3994eeb0
Alexa Drury <Windows Live ID\a_drury@hotmail.com>
2015-11-03 20:20:48 +1100

    Merged back to trunk

--- TradeYourPhoneDatabase/dbo/Tables/PhoneStatusHistory.sql
+++ TradeYourPhoneDatabase/dbo/Tables/PhoneStatusHistory.sql
@@ -5,9 +5,9 @@
     [PhoneStatusId] INT NOT NULL, 
     [StatusDate] DATETIME NOT NULL, 
     [CreatedBy] NVARCHAR(128) NOT NULL, 
-    CONSTRAINT [FK_PhoneStatusHistory_ToPhone] FOREIGN KEY ([PhoneId]) REFERENCES [Phone]([Id]), 
     CONSTRAINT [FK_PhoneStatusHistory_ToPhoneStatus] FOREIGN KEY ([PhoneStatusId]) REFERENCES [PhoneStatus]([Id]), 
-    CONSTRAINT [FK_PhoneStatusHistory_ToUser] FOREIGN KEY ([CreatedBy]) REFERENCES [AspNetUsers]([Id])
+    CONSTRAINT [FK_PhoneStatusHistory_ToUser] FOREIGN KEY ([CreatedBy]) REFERENCES [AspNetUsers]([Id]), 
+    CONSTRAINT [FK_PhoneStatusHistory_ToPhone] FOREIGN KEY ([PhoneId]) REFERENCES [Phone]([Id]) ON DELETE CASCADE
 )
 
 GO
